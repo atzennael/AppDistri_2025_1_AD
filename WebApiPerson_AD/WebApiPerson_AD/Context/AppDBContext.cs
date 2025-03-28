4af87a8a1a0a436c7345809bebd170af591e1cd6
Aedra Delgado <xsshiroinam@gmail.com>
2025-03-28 13:22:50 -0500

    Subir API

---
+++ WebApiPerson_AD/WebApiPerson_AD/Context/AppDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiPerson_AD.Models;
+
+namespace WebApiPerson_AD.Context
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options): base (options)
+        {
+            
+        }
+        public DbSet<Person> Persons{ get; set;}
+    }
+}
---
+++ WebApiPerson_AD/WebApiPerson_AD/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiPerson_AD.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// string de conexion
+var connectionString = builder.Configuration.GetConnectionString("Connection");
+
+//registrar servicio para la conexión
+builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
